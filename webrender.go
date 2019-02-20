6a7221b2a837547e60f3c204c97cb6732828a795
Nicholas LaRosa <nicholas@larosa.com>
2019-02-19 19:27:10 -0500

    add vendoring and simple hello

--- webrender.go
+++ webrender.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+	"os"
+	"github.com/Sirupsen/logrus"
+)
+
+var (
+	log = logrus.WithField("cmd", "webrender")
+)
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is where the websocket connects."))
+}
+
 func main() {
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.WithField("PORT", port).Fatal("$PORT must be set")
+	}
+
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/ws", handleHello)
+	log.Println(http.ListenAndServe(":"+port, nil))
 }
