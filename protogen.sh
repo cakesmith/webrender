61954a9658cf2e75cefcdc60a1c0ff6401014715
Nicholas LaRosa <nicholas@larosa.com>
2019-03-29 01:52:36 -0400

    add some cleanup to protobuf script

--- protogen.sh
+++ protogen.sh
@@ -4,4 +4,5 @@ IMAGE_ID=$(docker build --rm --target protoc -q -t foo . 2>/dev/null)
 docker run --cidfile=".cidfile" --entrypoint "/bin/true" $IMAGE_ID
 docker cp $(cat .cidfile):/protos/go protos
 docker cp $(cat .cidfile):/protos/js protos
+docker rm $(cat .cidfile) 1>/dev/null
 rm .cidfile
